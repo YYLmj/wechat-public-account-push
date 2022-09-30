2dc128a8895752593c84014c9243d4692384c89f
YYLmj <114717926+YYLmj@users.noreply.github.com>
2022-09-30 20:50:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,6 +13,8 @@ const USER_CONFIG = {
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: '45ae7ed16f8d1c9b1e11f9fac4e799db',
+  templareId:"ralwSAyM6CHhtcS54yhxaap2zRq3CH4A6J-epTBQAZA	",
+  user:["os5sX6CL0iqu63GNGn4IqwbzLOtw"，"os5sX6JMDGRVyPektFqTVlL6yias"],
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
